fa51be1d155408e7e25ea8ea09074e6bb89f5cf7
dkotin02 <danielkotin@gmail.com>
2014-01-08 23:33:51 -0500

    Added and updated references to sources. Added fixed styling to improve appearance

--- js/Script.js
+++ js/Script.js
@@ -1,6 +1,24 @@
-/** My simple implementation of identicon **/
+/** Identicon playgound **/
+/***************************
+Give credit where it is due:
+Original identicon idea by Don Park (and the source of identicon_canvas.js used in this example: https://github.com/donpark/identicon/
+Pretty neat implementation by Francis Shanahan and the source of identicon5.js : http://francisshanahan.com/index.php/identicon5 )
+ 
+MD5 jquery plugin source: https://github.com/gabrieleromanato/jQuery-MD5
+*****************************/
+$(document).ready(function () {
+    //Perform Don Park's canvas solution's tasks
+    render_identicon_canvases('donpark ');
+});
 
-$(document).ready(function() {
-    //What do do when loading is complete
-    $('li').identicon5();
+
+$('#make-identicon').click(function () {
+    //On click, generate hash and populate field
+    var input = $('#user-input').text();
+    //ideally, do some basic input checking here to catch empty input, etc.
+    var md5hash = $.md5(input);
+    $("#input-hash").text(md5hash);
+    $('#identicon5-test').text(md5hash);
+    //Perform Identicon5 tasks
+    $('#identicon5-test').identicon5({ size: 50 });
 });
