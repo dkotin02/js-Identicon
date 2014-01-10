b6b8091224a45e2da92ef4555fd27a805e3f8167
dkotin02 <danielkotin@gmail.com>
2014-01-09 21:48:03 -0500

    Don's solution now uses the user input. Release 0.1

--- README.md
+++ README.md
@@ -1,11 +1,8 @@
 js-Identicon
 ============
 
-Identicon generator in javascript
+Identicon generator in javascript using the HTML5 canvas element
+
 Will eventually make into a jQuery plugin for the curious minds and add more complexity
 
-Goals for v0.1:
-Input:
-32-bit hash (your pick)
-border size
 
--- js/Script.js
+++ js/Script.js
@@ -8,7 +8,7 @@ MD5 jquery plugin source: https://github.com/gabrieleromanato/jQuery-MD5
 *****************************/
 $(document).ready(function () {
     //Perform Don Park's canvas solution's tasks
-    render_identicon_canvases('donpark ');
+  //  render_identicon_canvases('donpark');
 });
 
 
@@ -19,9 +19,13 @@ $('#make-identicon').click(function () {
     var md5hash = $.md5(input);
     $("#input-hash").text(md5hash);
     $('#identicon5-test').text(md5hash);
+    //Use Don Park's solution. This is a hacky solution because I don't want to change his code
+    var truncatedhashint = parseInt(md5hash.substring(0, 8),16);
+    $('#donpark-canvas').attr("title", "donpark " + truncatedhashint);
+    render_identicon_canvases('donpark');
     //Perform Identicon5 tasks
     $('#identicon5-test').identicon5({ size: 50 });
-    //My test
+    //My tasks
     make_identicon($("#dk-identicon"), md5hash);
     //show content:
     $('#solutions').addClass("fade-in");
@@ -31,7 +35,7 @@ $('#make-identicon').click(function () {
 
 function make_identicon(target, hash) {
     //target: the target object to hold the results
-    var grid_size = 100; //define the grid size
+    var grid_size = 5; //define the grid size (n x n )
     var border_size = 15; //unsued
     var square_size = 20; //the size of the squares that will be painted
     var step_size = square_size; //Reduce as necessary
@@ -45,13 +49,11 @@ function make_identicon(target, hash) {
     //Assign color from hashvalue
     var color = hash.substring(hash.length - 6); //get last characters = 24 bits (for color information)
     var canvas = document.createElement("canvas");
-    canvas.setAttribute("width", grid_size);
-    canvas.setAttribute("height", grid_size);
+    canvas.setAttribute("width", grid_size*square_size);
+    canvas.setAttribute("height", grid_size * square_size);
     var context = canvas.getContext("2d");
     context.fillStyle = "#" + color;
     // context.fillStyle = "#" + (parseInt("FFFFFF", 16) - parseInt(color, 16)).toString(16); //Inverted color if you please
-    //  context.fillRect(0, 0, square_size, square_size)
-    //  context.fillRect(0, step_size, square_size, square_size);
 
     var posx = 0;
     var posy = 0;
@@ -61,26 +63,27 @@ function make_identicon(target, hash) {
     var binaryhash = parseInt(truncatedhash, 16).toString(2);
     // context.fillRect(posx, posy, square_size, square_size);
     //Loop through the hash and assign a value
-    for (var i = 0; i < (((grid_size / square_size) * (grid_size / square_size)) / 2); i++) {
+    var max_steps = (grid_size % 2 == 0) ? ((grid_size * grid_size) / 2) : ((grid_size * (grid_size + 1)) / 2);
+
+    for (var i = 0; i < max_steps; i++) {
         if (binaryhash.substring(binaryhash.length - 1) == "1") {
             //turn pixel on!
             posx = x * (square_size);
             posy = y * (square_size);
             context.fillRect(posx, posy, square_size, square_size);
-            //  context.fillRect(0, 0, square_size, square_size);
             //Mirror image to other half
-            context.fillRect(grid_size - posx, posy, square_size, square_size);
+            context.fillRect((grid_size - x -1)*square_size, posy, square_size, square_size);
+           
         }
         // console.log(binaryhash); 
         binaryhash = binaryhash.substring(0, binaryhash.length - 1); //move to the next bit
 
         y += 1; //move down by one unit
-        if (y * square_size == grid_size) {
+        if (y == grid_size) {
             //Reached a border, loop around and move to the right
             x += 1;
             y = 0;
         }
-        // console.log("doing stuff");
     }
 
     //write the result back to document
